0b39c05ffa216766082e32e3e9eb5e545f398bfc
linereseau <gayealioune11@gmail.com>
2022-01-26 10:24:19 +0100

    mjtp4

--- tp4/src/main/java/com/stepinfo/controller/HomeController.java
+++ tp4/src/main/java/com/stepinfo/controller/HomeController.java
@@ -9,12 +9,15 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.Controller;
 
+
 public class HomeController implements Controller {
 
    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
 	   
-       return new ModelAndView("/WEB-INF/mes_pages/home.jsp");
+       ModelAndView mv = new ModelAndView();
+       mv.setViewName("home.jsp");
+	return mv;
    }
 
 }
