793fdc3093f3178a1926a4dfba5759ef42624f8d
linereseau <gayealioune11@gmail.com>
2022-01-28 02:25:37 +0100

    Update TP4

--- tp4/src/main/java/com/stepinfo/controller/HomeController.java
+++ tp4/src/main/java/com/stepinfo/controller/HomeController.java
@@ -7,16 +7,17 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
 import org.springframework.web.servlet.mvc.Controller;
 
-
+@org.springframework.stereotype.Controller
 public class HomeController implements Controller {
 
    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
 	   
        ModelAndView mv = new ModelAndView();
-       mv.setViewName("home.jsp");
+       mv.setViewName("/WEB-INF/mes_pages/home.jsp");
 	return mv;
    }
 
