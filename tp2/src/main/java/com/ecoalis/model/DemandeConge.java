b7e4c000a31e7e0ca6fe253fa040192a21f77c07
linereseau <gayealioune11@gmail.com>
2022-01-30 23:30:45 +0100

    tp2

--- tp2/src/main/java/com/ecoalis/model/DemandeConge.java
+++ tp2/src/main/java/com/ecoalis/model/DemandeConge.java
@@ -1,21 +1,17 @@
 package com.ecoalis.model;
-
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
 public class DemandeConge {
-
    private Collaborateur collaborateur;
    private Periode periode;
 
    public Collaborateur getCollaborateur() {
       return collaborateur;
    }
-
    public void setCollaborateur(Collaborateur collaborateur) {
       this.collaborateur = collaborateur;
    }
-
    public Periode getPeriode() {
       return periode;
    }
